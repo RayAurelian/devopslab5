20d25973457539a17c3bc83ea34d492cc5dc874c
RayAurelian <mrhero228@gmail.com>
2026-02-19 11:42:53 +0300

    проверяем пайплайн

--- tests/test_user.py
+++ tests/test_user.py
@@ -26,16 +26,46 @@ def test_get_existed_user():
 
 def test_get_unexisted_user():
     '''Получение несуществующего пользователя'''
+    response = client.get("/api/v1/user", params={'email': users[0]['email']})
+    assert response.status_code == 404
+    assert response.json() == {"detail":"User not found"}
     pass
 
 def test_create_user_with_valid_email():
     '''Создание пользователя с уникальной почтой'''
-    pass
+    new_user = {'name': 'John Doe', 'email': 'jdoe@gmail.com'}
+    response = client.post("/api/v1/user", json=new_user)
+    assert response.status_code == 201
+    user_id = response.json()
+    assert isinstance(user_id, int)
+    '''проверка'''
+    responce = client.get("/api/v1/user", params={'email': new_user['email']})
+    assert responce.status_code == 200
+    assert responce.json()['name'] == new_user['name']
+    assert responce.json()['email'] == new_user['email']
 
 def test_create_user_with_invalid_email():
     '''Создание пользователя с почтой, которую использует другой пользователь'''
+    existing_email = users[0]['email']
+    response = client.post("/api/v1/user", json={'name': 'Duplicate', 'email': existing_email})
+    assert response.status_code == 409
+    assert response.json() == {"detail": "User with this email already exists"}
     pass
 
 def test_delete_user():
     '''Удаление пользователя'''
+    '''создаем временного юзера, тк иначе придется сначала создавать нового как выше, а порядок тестов может различаться '''
+    temp_user = {'name': 'for Kill', 'email': 'kill@mail.com'}
+    response = client.post("/api/v1/user", json=temp_user)
+    temp_email = temp_user['email']
+    assert response.status_code == 201
+
+    '''удаляем '''
+    del_resp = client.delete("/api/v1/user", params={'email': temp_email})
+    assert del_resp.status_code == 204
+
+   '''проверяем'''
+    del_resp = client.get("/api/v1/user", params={'email': temp_email})
+    assert del_resp.status_code == 404
+    assert del_resp.json() == {"detail": "User not found"}
     pass
